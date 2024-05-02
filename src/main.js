a42ffc8a312bb6e0d6c46d75b27df6eb54c87722
marusyaby <mariakhrol@gmail.com>
2024-05-02 17:35:06 +0300

    Add trip events item view

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import HeaderView from './view/header-view.js';
 import MainView from './view/main-view.js';
 import TripSortView from './view/trip-sort-view.js';
 import TripEventsListView from './view/trip-events-list-view.js';
+import TripEventsItemView from './view/trip-events-item-view.js';
 
 render(new HeaderView(), document.body);
 render(new MainView(), document.body);
@@ -10,4 +11,7 @@ render(new MainView(), document.body);
 const tripEventsContainerElement = document.querySelector('.trip-events');
 render(new TripSortView(), tripEventsContainerElement);
 render(new TripEventsListView(), tripEventsContainerElement);
+
+const tripEventsListContainerElement = document.querySelector('.trip-events__list');
+render(new TripEventsItemView(), tripEventsListContainerElement);
 
