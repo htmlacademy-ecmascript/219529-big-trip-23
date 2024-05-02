3b090a8c8c6bc20fb6c00aa5fcd375f8481dd30b
marusyaby <mariakhrol@gmail.com>
2024-05-02 18:02:13 +0300

    Add trip events item edit and create views

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import MainView from './view/main-view.js';
 import TripSortView from './view/trip-sort-view.js';
 import TripEventsListView from './view/trip-events-list-view.js';
 import TripEventsItemView from './view/trip-events-item-view.js';
+import TripEventsItemEditView from './view/trip-events-item-edit-view.js';
+import TripEventsItemCreateView from './view/trip-events-item-create-view.js';
 
 render(new HeaderView(), document.body);
 render(new MainView(), document.body);
@@ -13,5 +15,7 @@ render(new TripSortView(), tripEventsContainerElement);
 render(new TripEventsListView(), tripEventsContainerElement);
 
 const tripEventsListContainerElement = document.querySelector('.trip-events__list');
+render(new TripEventsItemCreateView(), tripEventsListContainerElement);
+render(new TripEventsItemEditView(), tripEventsListContainerElement);
 render(new TripEventsItemView(), tripEventsListContainerElement);
 
