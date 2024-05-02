e282b73e5d29d82da7aa326b8b677ca73a041159
marusyaby <mariakhrol@gmail.com>
2024-05-02 17:08:47 +0300

    Add events list view

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import {render} from './render.js';
 import NewHeaderView from './view/new-header-view.js';
 import NewMainView from './view/new-main-view.js';
 import NewTripSortView from './view/new-trip-sort-view.js';
+import EventsListView from './view/events-list-view.js';
 
 render(new NewHeaderView(), document.body);
 render(new NewMainView(), document.body);
 
 const tripEventsContainerElement = document.querySelector('.trip-events');
 render(new NewTripSortView(), tripEventsContainerElement);
+render(new EventsListView(), tripEventsContainerElement);
+
